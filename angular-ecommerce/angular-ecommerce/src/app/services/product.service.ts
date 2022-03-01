79f6a7261cdb1a779451136bfa128afba6e2d65d
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-01 21:57:49 +0100

    x

--- angular-ecommerce/angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl='http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(respone => respone._embedded.products)
+    );
+  }
+}
+
+interface GetResponse{
+  _embedded: {
+    products: Product[];
+  }
 }
