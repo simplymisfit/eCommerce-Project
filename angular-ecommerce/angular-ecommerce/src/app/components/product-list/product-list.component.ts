0764631ca9af1ab4215e8705d25c8cacceb4e265
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-06 16:57:45 +0100

    x

--- angular-ecommerce/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent implements OnInit {
   //new properties for pagination
 
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
   constructor(private productService: ProductService, 
@@ -78,11 +78,18 @@ export class ProductListComponent implements OnInit {
   }
 
   processResult(){
-    return data => {
+    return (data: any) => {
       this.products = data._embedded.products;
       this.thePageNumber = data.page.number + 1;
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
     };
   }
+
+  updatePageSize(pageSize: number){
+    this.thePageSize = pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+
 }
