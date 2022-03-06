2c6aef2e6ccfe8b686d961487a8058c999d7e862
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-06 17:13:04 +0100

    x

--- angular-ecommerce/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -21,6 +21,8 @@ export class ProductListComponent implements OnInit {
   thePageSize: number = 5;
   theTotalElements: number = 0;
 
+  previousKeyword: string = "";
+
   constructor(private productService: ProductService, 
               private route: ActivatedRoute) { }
 
@@ -44,12 +46,20 @@ export class ProductListComponent implements OnInit {
   handleSearchProducts(){
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
 
+    //if we have a different keyword than previous
+    //then set thePageNumber to 1
+    if (this.previousKeyword != theKeyword){
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+
+    console.log(`keyword: ${theKeyword}, thePageNumber: ${this.thePageNumber}`);
+
     //now search for the products using keyword
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    );    
+    this.productService.searchProductsPaginate(this.thePageNumber -1,
+                                      this.thePageSize,
+                                      theKeyword).subscribe(this.processResult());
   }
 
   handleListProducts(){
