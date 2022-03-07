cf3d3ca52c76aa5cbd99dba47e892a040b117e6a
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-07 18:12:31 +0100

    x

---
+++ angular-ecommerce/angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem){
+    //check if we already have the item in our cart
+
+
+    //find the item in the cart based on item id
+
+
+    //check if we found it
+  }
+}
