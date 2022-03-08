6efc0929284c268eb72c14c8e7e06f08cdf03462
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-08 17:24:15 +0100

    x

--- angular-ecommerce/angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/angular-ecommerce/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {path: 'products/:id', component: ProductDetailsComponent},
@@ -41,7 +42,8 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
