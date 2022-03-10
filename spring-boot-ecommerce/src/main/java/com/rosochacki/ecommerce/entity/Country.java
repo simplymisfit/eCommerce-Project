55934d15bd5815abb62a3e0686d72438ece90990
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-10 11:09:29 +0100

    x

--- spring-boot-ecommerce/src/main/java/com/rosochacki/ecommerce/entity/Country.java
+++ spring-boot-ecommerce/src/main/java/com/rosochacki/ecommerce/entity/Country.java
@@ -1,9 +1,11 @@
 package com.rosochacki.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="country")
@@ -21,4 +23,7 @@ public class Country {
     private String name;
 
     //Set up one-to-any with states
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
 }
