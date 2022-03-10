bf48d86e67ecabccbe96dc9a9d3382d0bb6d659c
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-10 11:34:46 +0100

    x

--- angular-ecommerce/angular-ecommerce/src/app/services/rosochacki-shop-form.service.ts
+++ angular-ecommerce/angular-ecommerce/src/app/services/rosochacki-shop-form.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Country } from '../common/country';
+import { map } from 'rxjs/operators';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +14,21 @@ export class RosochackiShopFormService {
   private statesUrl = 'http://localhost:8080/api/states';
 
   constructor(private httpClient: HttpClient) { }
+
+  getCountries(): Observable<Country[]>{
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]>{
+
+    //search url
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]>{
     let data: number[] = [];
@@ -39,5 +57,17 @@ export class RosochackiShopFormService {
 
     //the of operator from rxjs will wrap an object as an Observable
     return of(data);
+  }
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
   }
 }
