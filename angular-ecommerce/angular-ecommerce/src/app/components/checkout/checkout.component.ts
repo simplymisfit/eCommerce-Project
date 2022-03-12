add138306a04017b1f3281760dc76be3d16fd180
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-12 20:13:00 +0100

    x

--- angular-ecommerce/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -88,6 +88,11 @@ export class CheckoutComponent implements OnInit {
   onSubmit() {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
+
+    console.log("The email address is: " + this.checkoutFormGroup.get('customer').value.email);
+
+    console.log("The shipping address country  is: " + this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log("The shipping address state is: " + this.checkoutFormGroup.get('shippingAddress').value.state.name);
   }
 
   copyShippingAddressToBillingAddress(event){
