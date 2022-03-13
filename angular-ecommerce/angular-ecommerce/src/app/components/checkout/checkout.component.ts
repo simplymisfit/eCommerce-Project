19becbe3243909543253b5274a27052c63750a36
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-13 12:26:26 +0100

    adding validators

--- angular-ecommerce/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { RosochackiShopFormService } from 'src/app/services/rosochacki-shop-form.service';
@@ -30,9 +30,9 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -83,6 +83,10 @@ export class CheckoutComponent implements OnInit {
         this.countries = data;
       }
     );
+  }
+
+  get firstName(){
+    return this.checkoutFormGroup.get('customer.firstName');
   }
 
   onSubmit() {
