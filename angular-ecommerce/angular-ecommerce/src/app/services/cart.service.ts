33377e78244b9f5208ea20a41ff53c7a16e546b1
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-16 20:10:40 +0100

    x

--- angular-ecommerce/angular-ecommerce/src/app/services/cart.service.ts
+++ angular-ecommerce/angular-ecommerce/src/app/services/cart.service.ts
@@ -3,37 +3,42 @@ import { Subject } from 'rxjs';
 import { CartItem } from '../common/cart-item';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-  
   cartItems: CartItem[];
 
+  //Subject is a subclass of Observable and we can use it to publish events in code
   totalPrice: Subject<number> = new Subject<number>();
   totalQuantity: Subject<number> = new Subject<number>();
 
-  constructor() { 
-    this.cartItems = JSON.parse(sessionStorage.getItem('cartItems')) != null ? JSON.parse(sessionStorage.getItem('cartItems')):[];
+  constructor() {
+    this.cartItems =
+      JSON.parse(sessionStorage.getItem('cartItems')) != null
+        ? JSON.parse(sessionStorage.getItem('cartItems'))
+        : [];
   }
 
-  persistCartItems(){
+  persistCartItems() {
     sessionStorage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
-  addToCart(theCartItem: CartItem){
+  addToCart(theCartItem: CartItem) {
     //check if we already have the item in our cart
     let alreadyExistsInCart: boolean = false;
     let existingCartItem: CartItem = undefined!;
 
     if (this.cartItems.length > 0) {
       //find the item in the cart based on item id
-      existingCartItem = this.cartItems.find(tempCartItem => (tempCartItem.id === theCartItem.id));
+      existingCartItem = this.cartItems.find(
+        (tempCartItem) => tempCartItem.id === theCartItem.id
+      );
 
       //check if we found it
-      alreadyExistsInCart = (existingCartItem != undefined);
+      alreadyExistsInCart = existingCartItem != undefined;
     }
 
-    if (alreadyExistsInCart){
+    if (alreadyExistsInCart) {
       //increment the quantity
       existingCartItem.quantity++;
     } else {
@@ -43,13 +48,12 @@ export class CartService {
 
     //compute cart total price and total quantity
     this.computeCartTotals();
-
   }
   computeCartTotals() {
     let totalPriceValue: number = 0;
     let totalQuantityValue: number = 0;
 
-    for (let currentCartItem of this.cartItems){
+    for (let currentCartItem of this.cartItems) {
       totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
       totalQuantityValue += currentCartItem.quantity;
     }
@@ -65,18 +69,22 @@ export class CartService {
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
     console.log(`Contents of the cart`);
-    for (let tempCartItem of this.cartItems){
+    for (let tempCartItem of this.cartItems) {
       const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
       console.log(`name: ${tempCartItem.name}, quantity: ${tempCartItem.quantity}, 
                     unitPrice: ${tempCartItem.unitPrice}, subTotalPrice: ${subTotalPrice}`);
     }
-    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
+    console.log(
+      `totalPrice: ${totalPriceValue.toFixed(
+        2
+      )}, totalQuantity: ${totalQuantityValue}`
+    );
   }
 
   decrementQuantity(theCartItem: CartItem) {
     theCartItem.quantity--;
 
-    if (theCartItem.quantity === 0){
+    if (theCartItem.quantity === 0) {
       this.remove(theCartItem);
     } else {
       this.computeCartTotals();
@@ -84,7 +92,9 @@ export class CartService {
   }
 
   remove(theCartItem: CartItem) {
-    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+    const itemIndex = this.cartItems.findIndex(
+      (tempCartItem) => tempCartItem.id === theCartItem.id
+    );
     if (itemIndex > -1) {
       this.cartItems.splice(itemIndex, 1);
 
