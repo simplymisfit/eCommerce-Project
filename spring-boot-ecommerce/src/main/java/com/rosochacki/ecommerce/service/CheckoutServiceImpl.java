3bcae2f0906751d6ff27a3217640dc646035d1c7
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-17 17:37:00 +0100

    x

--- spring-boot-ecommerce/src/main/java/com/rosochacki/ecommerce/service/CheckoutServiceImpl.java
+++ spring-boot-ecommerce/src/main/java/com/rosochacki/ecommerce/service/CheckoutServiceImpl.java
@@ -3,8 +3,15 @@ package com.rosochacki.ecommerce.service;
 import com.rosochacki.ecommerce.dao.CustomerRepository;
 import com.rosochacki.ecommerce.dto.Purchase;
 import com.rosochacki.ecommerce.dto.PurchaseResponse;
+import com.rosochacki.ecommerce.entity.Customer;
+import com.rosochacki.ecommerce.entity.Order;
+import com.rosochacki.ecommerce.entity.OrderItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -17,7 +24,36 @@ public class CheckoutServiceImpl implements CheckoutService{
     }
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+        //retrieve the order from the dto
+        Order order = purchase.getOrder();
+
+        //generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        //populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        //populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        //populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        //save to the database
+        customerRepository.save(customer);
+
+        //return a response
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+        //generate a random UUID
+        return UUID.randomUUID().toString();
     }
 }
