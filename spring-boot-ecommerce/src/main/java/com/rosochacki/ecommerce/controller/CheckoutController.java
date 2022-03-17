cb1d7a0b947b479f552105d3911a82bdbb289f45
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-17 17:46:33 +0100

    x

--- spring-boot-ecommerce/src/main/java/com/rosochacki/ecommerce/controller/CheckoutController.java
+++ spring-boot-ecommerce/src/main/java/com/rosochacki/ecommerce/controller/CheckoutController.java
@@ -1,8 +1,24 @@
 package com.rosochacki.ecommerce.controller;
 
-import org.springframework.stereotype.Controller;
+import com.rosochacki.ecommerce.dto.Purchase;
+import com.rosochacki.ecommerce.dto.PurchaseResponse;
+import com.rosochacki.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
 public class CheckoutController {
-    
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+        return purchaseResponse;
+    }
 }
