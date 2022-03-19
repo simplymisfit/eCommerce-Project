fdfe63f1e3822dc8f497335ca0446af35e96f32b
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-19 14:53:53 +0100

    x

--- angular-ecommerce/angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/angular-ecommerce/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path: 'products/:prodId/:catId', component: ProductDetailsComponent},
@@ -36,7 +37,8 @@ const routes: Routes = [
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-ecommerce/angular-ecommerce/src/app/components/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import myAppConfig from 'src/app/config/my-app-config';
+import { OktaAuth } from '@okta/okta-auth-js';
+import * as OktaSignIn from '@okta/okta-signin-widget';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  oktaSignin: any;
+
+  constructor(private oktaAuthService: OktaAuth) {
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes,
+      },
+    });
+  }
+
+  ngOnInit(): void {}
+}
